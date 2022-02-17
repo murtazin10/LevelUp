cd28c87174dd10771d9b1a7a51efbbb75d491a66
Ильнар Муртазин <ilnarik10@bk.ru>
2022-02-17 21:59:57 +0300

    Сделал: из десятичной системы в двоичную

--- Homework8/Homework8/main.cpp
+++ Homework8/Homework8/main.cpp
@@ -1,9 +1,38 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int n;
-    printf("Enter n = ");
-    scanf("n = %d", &n);
+    long int N;
+    printf("Please enter N:\n");
+    scanf("%ld", &N);
 
+    long int array[100];
+    for (int k = 0; k < 256; ++k)
+        array[k] = 0;
+
+    long int j = 0;
+    do
+    {
+        long int P;
+        P = N % 2;
+        N = N / 2;
+        array[j] = P;
+        printf("array_%ld = %ld\n", j, array[j]);
+        ++j;
+    } while(N > 0);
+
+    long int arrayLength;
+    arrayLength = j;
+
+    printf("arrayLength = %ld\n", arrayLength);
+
+    long int binary_num;
+    binary_num = 0;
+    for (long int q = 0; q <= arrayLength - 1; ++q)
+    {
+        binary_num += array[arrayLength - 1 - q] * long(int(pow(10, arrayLength - 1 - q)));
+    }
+
+    printf("binary_num = %ld\n", binary_num);
 }
