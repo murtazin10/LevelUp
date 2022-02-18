3a4dc9c7a82ecb92026581c83f6064258fd258d6
Ильнар Муртазин <ilnarik10@bk.ru>
2022-02-18 23:44:11 +0300

    Done: Поменял вывод k - строки/стобца сразу на вывод матрицы ДО и ПОСЛЕ

--- Homewrok13/Homework13/main.cpp
+++ Homewrok13/Homework13/main.cpp
@@ -16,54 +16,58 @@ int main()
         }
     }
 
+    printf("The Initial Matrix: \n");
+    srand(time(NULL));
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            A[i][j] = rand() / 1e4;
+        }
+    }
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            printf(" %d", A[i][j]);
+        }
+        printf("\n");
+    }
+
     int k;
-    //printf("Please enter k no more than size = 7: ");
-    //scanf("%d\n", &k);
-    k = 5;
-
-    for (int n = 0; n < size; ++n)
-        printf("A_%d_%d = %d\n", n, k, A[n][k]);
-
-    printf("\n");
-
-    for (int t = 0; t < size; ++t)
-        printf("A_%d_%d = %d\n", k, t, A[k][t]);
-
-    printf("\n");
-    printf("Transposed column\n");
-    printf("\n");
+    // printf("Please enter k no more than size = 7: ");
+    // scanf("%d\n", &k);
+    k = 3;
 
     int C[size];
     for (int m = 0; m < size; ++m)
     {
         C[m] = A[m][k];
-        // printf("C_%d = %d\n", m, C[m]);
     }
-
-    // printf("\n");
 
     int P[size];
     for (int q = 0; q < size; ++q)
     {
         P[q] = A[k][q];
-        // printf("P_%d = %d\n", q, P[q]);
     }
-
-    // printf("\n");
 
     for (int i = 0; i < size; ++i)
             A[i][k] = P[i];
 
-    for (int i = 0; i < size; ++i)
-        printf("A_%d_%d = %d\n", i, k, A[i][k]);
-
-    printf("\n");
+    printf("\nThe Matrix with Transposed column/row: \n\n");
 
     for (int j = 0; j < size; ++j)
             A[k][j] = C[j];
 
-    for (int j = 0; j < size; ++j)
-        printf("A_%d_%d = %d\n", k, j, A[k][j]);
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = 0; j < size; ++j)
+        {
+            printf(" %d", A[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 
